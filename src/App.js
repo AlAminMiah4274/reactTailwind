e8b639fcc155d735d207a53010c929450904412a
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-28 22:12:34 +0600

    created responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1 className='text-4xl underline'>this is tailwind css</h1>
       <p>these are tailwind paragraph</p>
     </div>
